1ca16e1140f765330ad03cbb31dea975f69c5b4e
Viktor Fritzon <viktor@woocode.com>
2022-01-15 11:07:20 +0100

    add invitee name to right side of topnav

--- app/routes/movie-nights/$movieNightId.tsx
+++ app/routes/movie-nights/$movieNightId.tsx
@@ -41,58 +41,54 @@ export default function MovieNightRoute() {
   }
 
   return (
-    <>
-      <div className="min-h-full">
-        <Disclosure as="nav" className="bg-white border-b border-gray-200">
-          {({ open }) => (
-            <>
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex justify-between h-16">
-                  <div className="flex">
-                    <div className="flex-shrink-0 flex items-center">
-                      <img className="block lg:hidden h-8 w-auto" src={logo} />
-                      <img className="hidden lg:block h-12 w-auto" src={logo} />
-                    </div>
-                    <div className="-my-px ml-6 flex space-x-8">
-                      {navigation.map((item) => (
-                        <NavLink
-                          key={item.name}
-                          to={item.to}
-                          className={({ isActive }) =>
-                            classNames(
-                              isActive
-                                ? "border-indigo-500 text-gray-900"
-                                : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                              "inline-flex items-center px-1 pt-1 border-b-2 text-md font-medium"
-                            )
-                          }
-                        >
-                          {item.name}
-                        </NavLink>
-                      ))}
-                    </div>
-                  </div>
-                </div>
+    <div className="min-h-full">
+      <Disclosure as="nav" className="bg-white border-b border-gray-200">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between h-16">
+            <div className="flex">
+              <div className="flex-shrink-0 flex items-center">
+                <img className="block h-12 w-auto" src={logo} />
               </div>
-            </>
-          )}
-        </Disclosure>
-
-        <div className="py-10">
-          <header>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold leading-tight text-gray-900">
-                {movieNight.name}
-              </h1>
+              <div className="-my-px ml-6 flex space-x-8">
+                {navigation.map((item) => (
+                  <NavLink
+                    key={item.name}
+                    to={item.to}
+                    className={({ isActive }) =>
+                      classNames(
+                        isActive
+                          ? "border-indigo-500 text-gray-900"
+                          : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                        "inline-flex items-center px-1 pt-1 border-b-2 text-md font-medium"
+                      )
+                    }
+                  >
+                    {item.name}
+                  </NavLink>
+                ))}
+              </div>
             </div>
-          </header>
-          <main>
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-              <Outlet />
+            <div className="inline-flex items-center px-1 pt-1 text-md font-medium">
+              {invitee.name}
             </div>
-          </main>
+          </div>
         </div>
+      </Disclosure>
+
+      <div className="py-10">
+        <header>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold leading-tight text-gray-900">
+              {movieNight.name}
+            </h1>
+          </div>
+        </header>
+        <main>
+          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <Outlet />
+          </div>
+        </main>
       </div>
-    </>
+    </div>
   );
 }
