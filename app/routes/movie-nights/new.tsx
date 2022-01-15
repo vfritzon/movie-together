7b2a90295a5e32899cdaea58b1e8b7a13f9887cb
Viktor Fritzon <viktor@woocode.com>
2022-01-15 13:59:52 +0100

    style new movie night page

--- app/routes/movie-nights/new.tsx
+++ app/routes/movie-nights/new.tsx
@@ -25,19 +25,99 @@ export const action: ActionFunction = async ({ request }) => {
   return createUserSession(invitee.id, `/movie-nights/${movieNight.id}`);
 };
 
-export default function NewMovieNight() {
+import logo from "~/images/logo.svg";
+
+export default function NewMovieNightRoute() {
   return (
-    <div>
-      <h1>Create a Movie Night</h1>
-      <Form method="post" reloadDocument>
-        <label>
-          Movie Night Name: <input type="text" name="movieNightName" />
-        </label>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-        <button type="submit">Create</button>
-      </Form>
-    </div>
+    <>
+      <div className="min-h-full flex">
+        <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div>
+              <img className="h-18 w-auto" src={logo} alt="Workflow" />
+              <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
+                Watch a Movie Together
+              </h2>
+            </div>
+
+            <div className="mt-8">
+              <div className="mt-6">
+                <Form reloadDocument method="post" className="space-y-6">
+                  <div>
+                    <label
+                      htmlFor="movieNightName"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Title
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="movieNightName"
+                        name="movieNightName"
+                        placeholder="Saturday Movie Night"
+                        type="text"
+                        required
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="space-y-1">
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Your name
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        placeholder="Bob"
+                        id="name"
+                        name="name"
+                        type="text"
+                        required
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <button
+                      type="submit"
+                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                    >
+                      Let's Go!
+                    </button>
+                  </div>
+                </Form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:block relative w-0 flex-1">
+          <img
+            className="absolute inset-0 h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1540224871915-bc8ffb782bdf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
   );
 }
+//export default function NewMovieNight() {
+//  return (
+//    <div>
+//      <h1>Create a Movie Night</h1>
+//      <Form method="post" reloadDocument>
+//        <label>
+//          Movie Night Name: <input type="text" name="movieNightName" />
+//        </label>
+//        <label>
+//          Your Name: <input type="text" name="name" />
+//        </label>
+//        <button type="submit">Create</button>
+//      </Form>
+//    </div>
+//  );
+//}
