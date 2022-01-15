13ee8e499129a6a78dce4b11539c892458fb162d
Viktor Fritzon <viktor@woocode.com>
2022-01-15 13:02:16 +0100

    landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,38 @@
 import { Link } from "remix";
 
-export default function Index() {
+export default function Example() {
   return (
-    <div>
-      <h1>Movie Together</h1>
-      <Link to="/movie-nights/new" prefetch="render">
-        Get started
-      </Link>
-    </div>
+    <main className="lg:relative">
+      <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
+        <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+            <span className="block xl:inline">Movie</span>{" "}
+            <span className="block text-red-600 xl:inline">Together</span>
+          </h1>
+          <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+            Get your friends and family together and decide which movie to
+            watch.
+          </p>
+          <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
+            <div className="rounded-md shadow">
+              <Link
+                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 md:py-4 md:text-lg md:px-10"
+                to="/movie-nights/new"
+                prefetch="render"
+              >
+                Get started
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+        <img
+          className="absolute inset-0 w-full h-full object-cover"
+          src="https://images.unsplash.com/photo-1512149177596-f817c7ef5d4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
+          alt=""
+        />
+      </div>
+    </main>
   );
 }
