4cd42f5bdabd85366f81ddefbf854350b0ed1705
Viktor Fritzon <viktor@woocode.com>
2022-01-15 10:55:58 +0100

    remove mobile styles

--- app/routes/movie-nights/$movieNightId.tsx
+++ app/routes/movie-nights/$movieNightId.tsx
@@ -3,7 +3,6 @@ import type { Invitee, MovieNight } from "@prisma/client";
 import { db } from "~/utils/db.server";
 import { getInviteeId } from "~/utils/session.server";
 import { Disclosure } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import logo from "~/images/logo.svg";
 
 type LoaderData = { movieNight: MovieNight; invitee: Invitee };
@@ -54,7 +53,7 @@ export default function MovieNightRoute() {
                       <img className="block lg:hidden h-8 w-auto" src={logo} />
                       <img className="hidden lg:block h-12 w-auto" src={logo} />
                     </div>
-                    <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                    <div className="-my-px ml-6 flex space-x-8">
                       {navigation.map((item) => (
                         <NavLink
                           key={item.name}
@@ -73,44 +72,8 @@ export default function MovieNightRoute() {
                       ))}
                     </div>
                   </div>
-
-                  <div className="-mr-2 flex items-center sm:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <MenuIcon
-                          className="block h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      )}
-                    </Disclosure.Button>
-                  </div>
                 </div>
               </div>
-
-              <Disclosure.Panel className="sm:hidden">
-                <div className="pt-2 pb-3 space-y-1">
-                  {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-indigo-50 border-indigo-500 text-indigo-700"
-                          : "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800",
-                        "block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                </div>
-              </Disclosure.Panel>
             </>
           )}
         </Disclosure>
